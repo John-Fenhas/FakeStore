53ccd62d0fe5167c0343169d294e0857cfe422b2
John Fenhas <johnfenhas@gmail.com>
2026-06-13 17:59:31 +0300

    installed Axios and used it for api call fn

--- src/utiles/api.js
+++ src/utiles/api.js
@@ -1,7 +1,6 @@
-export const fetchProducts = async () => {
-  const res = await fetch("https://fakestoreapi.com/products");
-  if (!res.ok) throw new Error("Failed to fetch products");
-  const data = await res.json();
+import axios from "axios";
 
+export const fetchProducts = async () => {
+  const { data } = await axios.get("https://fakestoreapi.com/products");
   return data;
 };
